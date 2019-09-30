4d66173ff75ebbe3518aee55b01ba0c0c4f9a606
nalbam <me@nalbam.com>
2019-09-30 20:47:58 +0900

    helm search

--- run.sh
+++ run.sh
@@ -299,7 +299,7 @@ helm_install() {
     fi
 
     # latest chart version
-    LATEST=$(helm search ${NAME} | grep ${REPO} | head -1 | awk '{print $2}')
+    LATEST=$(helm search "${NAME}" | grep "${REPO} " | head -1 | awk '{print $2}')
 
     if [ "${LATEST}" != "" ]; then
         _result "latest chart version: ${LATEST}"
